8d629a0222435a0a1182180723d31bb656bfe072
vukmilenkovic <vukmilenkovic9@gmail.com>
2025-09-25 09:23:06 +0200

    Added the html css and JS files to the project
    Created a getComputerChoice function that will randomly display a value between rock, paper, or scissors.

---
+++ index.js
@@ -0,0 +1,24 @@
+// Function that randomly returns one of the following values "rock", "paper", "scissors"
+function getComputerChoice(){
+    const rock = "rock"
+    const paper = "paper"
+    const scissors = "scissors"
+
+    // Generate a random number between 1 and 3 
+    let randomNum = Math.floor(Math.random() * 3) + 1
+
+    // Connect the random number to the appropriate variables
+    if (randomNum == 1){
+        console.log(rock)
+        return rock
+    } else if (randomNum == 2){
+        console.log(paper)
+        return paper
+    } else if (randomNum == 3) {
+        console.log(rock)
+        return rock
+    }
+    
+}
+
+// Function that takes user input and returns it 
