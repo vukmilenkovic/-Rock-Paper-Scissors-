d2cfdfa40dbae2420b14b51007f9497bc908ec68
vukmilenkovic <vukmilenkovic9@gmail.com>
2025-09-25 10:05:17 +0200

    Created the variables for keeping track of the score
    Created the variables humanChoice and computerChoice to save the values returned from the corresponding functions
    
    Created the playRound function that starts the game.

--- index.js
+++ index.js
@@ -25,6 +25,39 @@ function getComputerChoice(){
 function getHumanChoice(){
     // USe prompt to get the user inpurt 
     let input = prompt("Please choice what symbol you will use: ROCK, PAPER OR SCISSORS")
-    console.log(input)
+    // Set the human choice to lower case
+    return input.toLowerCase();
 }
+
+// Variables that track the human and computer scores
+let humanScore = 0;
+let computerScore = 0;
+
+// Game Logic
+// the game will be played round by round 
+// Function that takes human and computer choice as arguments, plays a single round, increments the round winner's score and logs winner announcment
+function playRound(humanChoice, computerChoice){
+    // Check if the values are equals, if equal it's a draw
+    if (humanChoice == computerChoice){
+        return "It's a draw!"
+    } else if (humanChoice == 'paper' && computerChoice == 'rock'){
+        humanScore += 1
+        console.log(humanChoice, computerChoice)
+        console.log("Human wins")
+    } else if (humanChoice == "rock" && computerChoice == "scissors"){
+        humanScore += 1
+        console.log(humanChoice, computerChoice)
+        console.log("Human wins")
+    } else if (humanChoice == "scissors" && computerChoice == "paper"){
+        humanScore += 1
+        console.log(humanChoice, computerChoice)
+        console.log("Human wins")
+    }
+}
+
+const humanChoice = getHumanChoice();
+const computerChoice = getComputerChoice();
+
+// TODO: Make the game logic so that it keeps track of the score revokes the playRound function.
+
 
