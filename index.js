ab7a6595e2fd666214ff1a5e39054fda0a492056
vukmilenkovic <vukmilenkovic9@gmail.com>
2025-09-25 22:12:20 +0200

    Made some slight changes to the game logic and the code
    Must upgrade the logic!

--- index.js
+++ index.js
@@ -1,24 +1,8 @@
 // Function that randomly returns one of the following values "rock", "paper", "scissors"
 function getComputerChoice(){
-    const rock = "rock"
-    const paper = "paper"
-    const scissors = "scissors"
-
-    // Generate a random number between 1 and 3 
-    let randomNum = Math.floor(Math.random() * 3) + 1
-
-    // Connect the random number to the appropriate variables
-    if (randomNum == 1){
-        console.log(rock)
-        return rock
-    } else if (randomNum == 2){
-        console.log(paper)
-        return paper
-    } else if (randomNum == 3) {
-        console.log(rock)
-        return rock
-    }
-    
+    const choices = ["Rock", "Paper", "Scissors"];
+    const randomIndex = Math.floor(Math.random() * choices.length);
+    return choices[randomIndex];
 }
 
 // Function that takes user input and returns it 
@@ -33,31 +17,51 @@ function getHumanChoice(){
 let humanScore = 0;
 let computerScore = 0;
 
+
+
+
 // Game Logic
 // the game will be played round by round 
 // Function that takes human and computer choice as arguments, plays a single round, increments the round winner's score and logs winner announcment
-function playRound(humanChoice, computerChoice){
+function playRound(){
+    let humanChoice = getHumanChoice();
+    let computerChoice = getComputerChoice();
+    // After each round reset the values for human and computer choices
     // Check if the values are equals, if equal it's a draw
     if (humanChoice == computerChoice){
         return "It's a draw!"
     } else if (humanChoice == 'paper' && computerChoice == 'rock'){
-        humanScore += 1
         console.log(humanChoice, computerChoice)
         console.log("Human wins")
+        return humanScore += 1;
     } else if (humanChoice == "rock" && computerChoice == "scissors"){
-        humanScore += 1
         console.log(humanChoice, computerChoice)
         console.log("Human wins")
+        return humanScore += 1;
     } else if (humanChoice == "scissors" && computerChoice == "paper"){
-        humanScore += 1
         console.log(humanChoice, computerChoice)
         console.log("Human wins")
+        return humanScore += 1;
+        
+    }
+    else {
+        console.log("Robot won this round!")
+        return computerScore += 1;
+
     }
 }
 
-const humanChoice = getHumanChoice();
-const computerChoice = getComputerChoice();
+
 
 // TODO: Make the game logic so that it keeps track of the score revokes the playRound function.
+// while(humanScore  < 3 || computerScore < 3) {
+//     playRound(humanChoice, computerChoice)
+// }
 
+// Anounce the winner 
+// if (humanScore == 3 ){
+//     console.log("Human won, better luch next time bot!");
+// } else if (computerScore == 3){
+//     console.log("Robot won, how can you allow this?")
+// }
 
