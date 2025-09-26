4efc90e6777ba3d55ebbdd65dcbd73d46e0be7f5
vukmilenkovic <vukmilenkovic9@gmail.com>
2025-09-26 10:21:12 +0200

    Edited  the JS code so that it runs correctly, changed  the playRound function so that the choices are correct. Changed the while loop so that it check correct paramethers, and added a if statement to print out the winner.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Function that randomly returns one of the following values "rock", "paper", "scissors"
 function getComputerChoice(){
-    const choices = ["Rock", "Paper", "Scissors"];
+    const choices = ["rock", "paper", "scissors"];
     const randomIndex = Math.floor(Math.random() * choices.length);
     return choices[randomIndex];
 }
@@ -26,42 +26,41 @@ let computerScore = 0;
 function playRound(){
     let humanChoice = getHumanChoice();
     let computerChoice = getComputerChoice();
-    // After each round reset the values for human and computer choices
-    // Check if the values are equals, if equal it's a draw
-    if (humanChoice == computerChoice){
-        return "It's a draw!"
-    } else if (humanChoice == 'paper' && computerChoice == 'rock'){
-        console.log(humanChoice, computerChoice)
-        console.log("Human wins")
-        return humanScore += 1;
-    } else if (humanChoice == "rock" && computerChoice == "scissors"){
-        console.log(humanChoice, computerChoice)
-        console.log("Human wins")
-        return humanScore += 1;
-    } else if (humanChoice == "scissors" && computerChoice == "paper"){
-        console.log(humanChoice, computerChoice)
-        console.log("Human wins")
-        return humanScore += 1;
-        
-    }
+    console.log(humanChoice);
+    console.log(computerChoice);
+    // Combination for user winning
+    if(humanChoice == "rock" && computerChoice == 'scissors' || humanChoice == "paper" && computerChoice == "rock" || humanChoice == "scissors" && computerChoice == "paper"){
+        console.log("You won this round! +1");
+        humanScore += 1;
+        console.log("Your score is " + humanScore);
+        console.log("Computer score is " + computerScore);
+        return 
+    } // Combination for computer winning
+    else if (humanChoice == "rock" && computerChoice == 'paper' || humanChoice == "paper" && computerChoice == "scissors" || humanChoice == "scissors" && computerChoice == "rock"){
+        console.log("Robot won this round! +1");
+        computerScore += 1;
+        console.log("Your score is " + humanScore);
+        console.log("Computer score is " + computerScore);
+        return 
+    } // Print out that it's a tie
     else {
-        console.log("Robot won this round!")
-        return computerScore += 1;
-
+        console.log("It's a tie, don't give up,  you can beat the machine! Just like in slots :)")
     }
+    
 }
 
 
 
 // TODO: Make the game logic so that it keeps track of the score revokes the playRound function.
-// while(humanScore  < 3 || computerScore < 3) {
-//     playRound(humanChoice, computerChoice)
-// }
+while(humanScore  < 3 && computerScore < 3) {
+   playRound();
+}
+
 
 // Anounce the winner 
-// if (humanScore == 3 ){
-//     console.log("Human won, better luch next time bot!");
-// } else if (computerScore == 3){
-//     console.log("Robot won, how can you allow this?")
-// }
+if (humanScore == 3 ){
+console.log("Human won, better luch next time bot!");
+} else if (computerScore == 3){
+console.log("Robot won, how can you allow this?")
+}
 
