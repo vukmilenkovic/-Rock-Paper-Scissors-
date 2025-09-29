226ec308f835c930e8de1d4d9996331fbc7c26b6
vukmilenkovic <vukmilenkovic9@gmail.com>
2025-09-29 08:11:51 +0200

    Fixed the game logic so that when the score is 3 the gameOver function is envoked
    Need to add changes to the gameOver function so that the document is updated correctly.

--- index.js
+++ index.js
@@ -14,22 +14,20 @@ function getHumanChoice(){
     return input
 }
 
-// Variables that track the human and computer scores
+// Define the scores
 let humanScore = 0;
 let computerScore = 0;
-
-
-
 
 // Game Logic
 // the game will be played round by round 
 // Function that takes human and computer choice as arguments, plays a single round, increments the round winner's score and logs winner announcment
 function playRound(){
+    // Define choices
     let humanChoice = getHumanChoice();
     let computerChoice = getComputerChoice();
     // Get the DOM element that displays current hand
-    document.querySelector(".human-current").textContent = humanChoice;
-    document.querySelector(".computer-current").textContent = computerChoice;
+    document.querySelector(".human-current").value = humanChoice;
+    document.querySelector(".computer-current").value = computerChoice;
     // Combination for user winning
     if(humanChoice == "rock" && computerChoice == 'scissors' || humanChoice == "paper" && computerChoice == "rock" || humanChoice == "scissors" && computerChoice == "paper"){
         console.log("You won this round! +1");
@@ -38,7 +36,6 @@ function playRound(){
         document.querySelector('.human-score').textContent = humanScore;
         console.log("Your score is " + humanScore);
         console.log("Computer score is " + computerScore);
-        return 
     } // Combination for computer winning
     else if (humanChoice == "rock" && computerChoice == 'paper' || humanChoice == "paper" && computerChoice == "scissors" || humanChoice == "scissors" && computerChoice == "rock"){
         console.log("Robot won this round! +1");
@@ -47,11 +44,20 @@ function playRound(){
         document.querySelector('.computer-score').textContent = computerScore;
         console.log("Your score is " + humanScore);
         console.log("Computer score is " + computerScore);
-        return 
     } // Print out that it's a tie
     else {
         console.log("It's a tie, don't give up,  you can beat the machine! Just like in slots :)")
         document.querySelector('.message').textContent = "It's a tie, don't give up,  you can beat the machine! Just like in slots :)";
+    }
+    // Check for winner
+    if (humanScore == 3 ) {
+        console.log("Human won, better luch next time bot!");
+        document.querySelector('.message').textContent = "Human won, better luch next time bot!";
+        gameOver();
+    } else if (computerScore == 3) {
+        console.log("Robot won, how can you allow this?")
+        document.querySelector('.message').textContent = "Robot won, how can you allow this?";
+        gameOver();
     }
 }
 
@@ -61,16 +67,7 @@ function gameOver(){
     gameContainer.style.backgroundColor = 'red';
 }
 
-// Anounce the winner 
-    if (humanScore == 3 ){
-    console.log("Human won, better luch next time bot!");
-    document.querySelector('.message').textContent = "Human won, better luch next time bot!";
-    gameOver();
-    } else if (computerScore == 3){
-    console.log("Robot won, how can you allow this?")
-    document.querySelector('.message').textContent = "Robot won, how can you allow this?";
-    gameOver();
-    }
+
 
 // Invoke the playRound() function when the user types in a text and presses enter
 document.addEventListener('DOMContentLoaded', () => {
